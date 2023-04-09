5f1331cf4409b92199b62254e95749d924942d00
Cclocuh <118848644+Cclocuh@users.noreply.github.com>
2023-04-08 22:43:53 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactorialConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingNumber = 6;
+            Console.Out.WriteLine(factorial(startingNumber));
+            Console.ReadLine();
+        }
+
+        static int factorial (int x)
+        {
+            Console.Out.WriteLine("x is {0}", x);
+            if (x == 1) 
+            {
+                return 1;
+            }
+            else
+            {
+                return x * factorial(x - 1);
+            }
+        }
+    }
+}
